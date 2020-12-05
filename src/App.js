82a551452f73dc0c3546433db927f2017fc74d11
aminul007bd <gem007bd@gmail.com>
2020-12-05 15:17:33 +0100

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        hello workd
+      </div>
       <h2>React Playground</h2>
     </div>
   );
 }
 
-export default App;
+export default App
