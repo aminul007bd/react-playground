17f34e857d15123bd3c69889846ffe23b3c567f1
aminul007bd <gem007bd@gmail.com>
2020-12-06 11:11:13 +0100

    add eslint in the project

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from 'react'
 import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <div className="container">
-
-        <h2>React Playground</h2>
-
-      </div>
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				<h2>React Playground</h2>
+			</div>
+		</div>
+	)
 }
 
 export default App
