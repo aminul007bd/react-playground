a622f308680d413014aeae7992ab1d3839b3ef05
aminul007bd <gem007bd@gmail.com>
2020-12-13 11:32:36 +0100

    fix search and use dangerouslySetInnerHTML

--- src/components/search/SearchList.js
+++ src/components/search/SearchList.js
@@ -3,10 +3,10 @@ import axios from 'axios'
 
 function SearchList({ searchQuery }) {
 	const baseUrl = 'https://en.wikipedia.org/w/api.php'
-	const [data, setData] = useState([])
+	const [results, setResults] = useState([])
 	useEffect(() => {
-		const fetchData = async () => {
-			const result = await axios.get(baseUrl, {
+		const searchWiki = async () => {
+			const { data } = await axios.get(baseUrl, {
 				params: {
 					action: 'query',
 					list: 'search',
@@ -16,16 +16,38 @@ function SearchList({ searchQuery }) {
 				},
 			})
 
-			setData(result.data.query?.search)
+			setResults(data.query?.search)
 		}
-		fetchData()
+		searchWiki()
 	}, [searchQuery])
-	console.log(data)
-	const searchResult = data.map((result) => {
-		return <li key={result.pageid}>{result.title}</li>
+	const searchResult = results?.map((result) => {
+		return (
+			<li
+				key={result.pageid}
+				className="list-group-item d-flex justify-content-between align-items-center"
+			>
+				<p>{result.title}</p>
+				<span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+				{/* <div className="image-parent">
+					<img
+						src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/don_quixote.jpg"
+						className="img-fluid"
+						alt={result.title}
+					/>
+				</div> */}
+			</li>
+		)
 	})
 
-	return <ul>{searchResult}</ul>
+	return (
+		<div className="container mt-4">
+			<div className="row">
+				<div className="col-12 col-sm-12">
+					<ul className="list-group">{searchResult}</ul>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default SearchList
