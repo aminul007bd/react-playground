ba060b489fa266546957f04fd1fdc17bbad094fa
aminul007bd <gem007bd@gmail.com>
2021-03-14 13:50:02 +0100

    change bootstrap

--- src/components/PretrainedIntents.js
+++ src/components/PretrainedIntents.js
@@ -3,9 +3,11 @@ import appData from '../data/intents.json'
 import { Link } from 'react-router-dom'
 
 function PretrainedIntents() {
-	const [isActive, setIsActive] = useState(false)
 	const [selectedIds, setSelectedIds] = useState([])
+	const [selectAll, setSelectAll] = useState(false)
+
 	console.log('selected ids: ', selectedIds)
+
 	const topRow = () => {
 		return (
 			<div className="row">
@@ -18,6 +20,15 @@ function PretrainedIntents() {
 			</div>
 		)
 	}
+
+	const selectAllIntents = () => {
+		const allIds = appData.map((intents) => {
+			return intents.id
+		})
+		console.log('all ids', allIds)
+		setSelectedIds(...selectedIds, allIds)
+	}
+
 	const intentsData = appData.map((intents) => {
 		const onToggle = (id) => {
 			if (selectedIds.includes(id)) {
@@ -28,6 +39,7 @@ function PretrainedIntents() {
 			}
 		}
 		const isRowSelected = selectedIds.includes(intents.id) ? 'active' : ''
+
 		return (
 			<div className="list-group mb-1" key={intents.id}>
 				<Link
@@ -40,7 +52,7 @@ function PretrainedIntents() {
 						<small>{intents.trainingData.expressionCount}</small>
 					</div>
 					{intents.trainingData.expressions.map((expression) => (
-						<span key={expression.id} className="badge badge-secondary badge-pill mr-1">
+						<span key={expression.id} className="badge badge-secondary mr-1">
 							{expression.text}
 						</span>
 					))}
@@ -53,8 +65,19 @@ function PretrainedIntents() {
 	})
 	return (
 		<>
-			{topRow}
-			{intentsData}
+			<div className="row">
+				<div className="row">
+					<div className="col-xs-6">
+						<h3>{`Total selected: ${selectedIds.length}`}</h3>
+					</div>
+					<div className="col-xs-6">
+						<button className="btn btn-outline-secondary" onClick={selectAllIntents}>
+							Select All
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className="row">{intentsData}</div>
 		</>
 	)
 }
