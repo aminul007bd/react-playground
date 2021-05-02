3deb71f77b5c1d858e4d621f3f0e146491b72a2f
aminul007bd <gem007bd@gmail.com>
2021-05-02 12:22:04 +0200

    fix prettierrc issue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './App.css'
+import AppFooter from './components/AppFooter'
+import AppHeader from './components/AppHeader'
 
 function App() {
 	return (
 		<div className="App">
 			<div className="container">
 				<h2>React Playground</h2>
+				<AppHeader />
+				<AppFooter />
 			</div>
 		</div>
 	)
