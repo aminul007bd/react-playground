84189a8ce31a5b24a01d865e085131c58ec87aee
aminul007bd <gem007bd@gmail.com>
2021-05-02 15:39:15 +0200

    fix itaration issue

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -4,11 +4,13 @@ import './Expense.css'
 function Expenses(props) {
 	return (
 		<div className="card expenses">
-			<ExpenseItem
-				title={props.items[0].title}
-				amount={props.items[0].amount}
-				date={props.items[0].date}
-			/>
+			{props.items.map((expenses) => (
+				<ExpenseItem
+					title={expenses.title}
+					amount={expenses.amount}
+					date={expenses.date}
+				/>
+			))}
 		</div>
 	)
 }
