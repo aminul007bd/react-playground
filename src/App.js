8784bc03fef431d22918d77d0a82420e37cc70c9
aminul007bd <gem007bd@gmail.com>
2021-05-25 23:22:50 +0200

    user management start project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React from 'react'
-import './App.css'
-import AppFooter from './components/AppFooter'
-import AppHeader from './components/AppHeader'
+import React, { useState } from 'react'
+
+import Login from './components/Login/Login'
+import Home from './components/Home/Home'
+import MainHeader from './components/MainHeader/MainHeader'
 
 function App() {
+	const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+	const loginHandler = (email, password) => {
+		// We should of course check email and password
+		// But it's just a dummy/ demo anyways
+		setIsLoggedIn(true)
+	}
+
+	const logoutHandler = () => {
+		setIsLoggedIn(false)
+	}
+
 	return (
-		<div className="App">
-			<div className="container">
-				<h2>React Playground</h2>
-				<AppHeader />
-				<AppFooter />
-			</div>
-		</div>
+		<React.Fragment>
+			<MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+			<main>
+				{!isLoggedIn && <Login onLogin={loginHandler} />}
+				{isLoggedIn && <Home onLogout={logoutHandler} />}
+			</main>
+		</React.Fragment>
 	)
 }
 
